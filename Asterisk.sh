81851923608932f748686fe9d31313f1c798680d
internal77 <73139884+internal77@users.noreply.github.com>
2021-02-11 10:51:08 +0200

    Add files via upload
    aster

---
+++ Asterisk.sh
@@ -0,0 +1,9 @@
+sudo apt-get install -y openssh-server apache2 mysql-server mysql-client \
+  mongodb curl sox mpg123 sqlite3 git uuid libodbc1 unixodbc unixodbc-bin \
+  asterisk asterisk-core-sounds-en-wav asterisk-core-sounds-en-g722 \
+  asterisk-dahdi asterisk-flite asterisk-modules asterisk-mp3 asterisk-mysql \
+  asterisk-moh-opsound-g722 asterisk-moh-opsound-wav asterisk-opus \
+  asterisk-voicemail dahdi dahdi-dkms dahdi-linux libapache2-mod-security2 \
+  php5.6 php5.6-cgi php5.6-cli php5.6-curl php5.6-fpm php5.6-gd php5.6-mbstring \
+  php5.6-mysql php5.6-odbc php5.6-xml php5.6-bcmath php-pear libicu-dev gcc \
+  g++ make postfix libapache2-mod-php5.6
